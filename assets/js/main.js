bb1d8438ff12b867e1ad7f691801b8eee2b8dec9
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-03-10 14:37:39 +0100

    add "if" inside "for" with ex. conditions

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,13 +5,25 @@
 */
 
 // strumenti
-// • for+
+// • for
 // • if/else
 // • operatore %
 
 // stampare in console i numeri da 1 a 100:
 
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
-    
+
+    // se il numero è divisibile per sia per 3 che per 5
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("FizzBuzz");
+        // se il numero è divisibile solo per 3
+      } else if (i % 3 == 0) {
+        console.log("Fizz");
+        // se il numero è divisibile solo per 5
+      } else if (i % 5 == 0) {
+        console.log("Buzz");
+        // se non è divisibile ne per 3 ne per 5
+      } else {
+        console.log(i);
+      }
 }
