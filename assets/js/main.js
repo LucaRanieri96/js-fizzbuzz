af3e997058367a1a4ffed26d4a5ba8656586d00b
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-03-10 14:48:35 +0100

    bonus1 write in dom

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,11 +4,20 @@
 • per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 */
 
-// strumenti
-// • for
-// • if/else
-// • operatore %
+ /* strumenti:
+ • for
+ • if/else
+ • operatore %
+ */
 
+ /* strumenti bonus 1:
+ • getElementById
+ • innerHTML
+
+ */
+// ho creato un div nel body, lo richiamo dentro una variabile con getelementbyid
+const container = document.getElementById("container");
+// ora aggiungo nel ciclo for il modo per scrivere in pagina attraverso questa costante
 // stampare in console i numeri da 1 a 100:
 
 for (let i = 1; i <= 100; i++) {
@@ -16,14 +25,22 @@ for (let i = 1; i <= 100; i++) {
     // se il numero è divisibile per sia per 3 che per 5
     if (i % 3 == 0 && i % 5 == 0) {
         console.log("FizzBuzz");
+
+        container.innerHTML = `<p>FizzBuzz</p>`
         // se il numero è divisibile solo per 3
       } else if (i % 3 == 0) {
         console.log("Fizz");
+
+        container.innerHTML = `<p>Fizz</p>`
         // se il numero è divisibile solo per 5
       } else if (i % 5 == 0) {
         console.log("Buzz");
+
+        container.innerHTML = `<p>Buzz</p>`
         // se non è divisibile ne per 3 ne per 5
       } else {
         console.log(i);
+        
+        container.innerHTML = `<p>${i}</p>`
       }
 }
